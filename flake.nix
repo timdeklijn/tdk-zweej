007a7e42bf8372e4ba860be5b9ee0af86ef9c4dc
Tim de Klijn <timdeklijn@gmail.com>
2026-09-22 10:16:06 +0200

    update environment

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Development shell for tdk-zweej";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.gh
+          ];
+        };
+      });
+    };
+}
